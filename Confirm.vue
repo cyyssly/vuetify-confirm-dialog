ff4883f4ab4ff27c5fefd3b73ecbdfbf3609313a
Lyde Su <cyyssly@sohu.com>
2019-03-14 17:42:10 +0800

    Update Confirm.vue

--- Confirm.vue
+++ Confirm.vue
@@ -2,17 +2,31 @@
   <v-layout row justify-center>
     <v-dialog v-model="isShow" persistent max-width="300">
       <v-card>
-        <v-card-title class="warning lighten-2 subheading">
+        <v-card-title class="lighten-2 subheading" :class="text.type">
           <v-icon id="homeicon" class="fas fa-exclamation-circle">&nbsp;&nbsp;{{text.title}}</v-icon>
         </v-card-title>
         <v-card-text class="subheading text-md-center mt-3">{{text.msg}}</v-card-text>
         <v-card-actions>
           <v-layout row justify-space-around>
-            <v-btn v-if="text.btn.ok" flat color="error" class="mb-2" @click="ok()">
-              <i class="fa fa-check"></i>&nbsp;&nbsp;{{text.btn.ok}}
+            <v-btn
+              v-if="text.btnyes.visible"
+              flat
+              :color="text.btnyes.color"
+              class="mb-2"
+              @click="ok()"
+            >
+              <v-icon small>{{text.btnyes.icon}}</v-icon>
+              &nbsp;&nbsp;{{text.btnyes.text}}
             </v-btn>
-            <v-btn v-if="text.btn.no" flat color="primary" class="mb-2" @click="close()">
-              <i class="fa fa-times"></i>&nbsp;&nbsp;{{text.btn.no}}
+            <v-btn
+              v-if="text.btnno.visible"
+              flat
+              :color="text.btnno.color"
+              class="mb-2"
+              @click="close()"
+            >
+              <v-icon small>{{text.btnno.icon}}</v-icon>
+              &nbsp;&nbsp;{{text.btnno.text}}
             </v-btn>
           </v-layout>
         </v-card-actions>
@@ -25,22 +39,15 @@ export default {
   data () {
     return {
       isShow: true,
-      text: {
-        title: '提示',
-        msg: '确定要删除吗？',
-        btn: {
-          ok: '确定',
-          no: '取消'
-        }
-      }
+      text: { btnyes: { visible: true }, btnno: { visible: true } }
     }
   },
   methods: {
-    ok () {
-      console.log('确定')
-    },
     close () {
       console.log('关闭')
+    },
+    ok () {
+      console.log('确定')
     }
   }
 }
